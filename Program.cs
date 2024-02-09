aeed89209804b0cda1c6824563cdbadc82a50dac
annas333 <annashendge333@gmail.com>
2024-02-09 21:27:24 +0530

    Added sum program

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GithubActions
@@ -10,6 +11,11 @@ namespace GithubActions
     {
         static void Main(string[] args)
         {
+            int x = 5;
+            int y = 6;
+            int sum = x + y;
+            Console.WriteLine(sum); // Print the sum of x + y
+            Thread.Sleep(5000);
         }
     }
 }
